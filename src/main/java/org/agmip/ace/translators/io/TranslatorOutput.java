b2cb251059e767151a16594b1510765ee6ea3b88
Christopher Villalobos <cvillalobos@ufl.edu>
2013-09-16 10:41:20 -0400

    Initial Commit

---
+++ src/main/java/org/agmip/ace/translators/io/TranslatorOutput.java
@@ -0,0 +1,25 @@
+package org.agmip.ace.translators.io;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.agmip.ace.AceBaseComponentType;
+import org.agmip.ace.AceDataset;
+
+/**
+ * The {@code TranslatorOutput} interface provides methods for converting ACE data to
+ * model-specific data.
+ */
+public interface TranslatorOutput {
+    /**
+     * Write the specified component(s) of an AceDataset to the <pre>outputDirectory</pre>
+     *
+     * This method writes out the specified component(s) an AceDataset to the
+     * <pre>outputDirectory</pre> in a model-specific format.
+     *
+     * @param  outputDirectory the directory to ouput the translated files.
+     * @param  dataset         the source ACE Dataset
+     * @param  ...             subcomponents to translate
+     */
+    public void write(File outputDirectory, AceDataset dataset, AceBaseComponentType... components) throws IOException;
+}
